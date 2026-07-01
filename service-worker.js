482e24161c0ac5aab24fb63527c988e62cb273b4
pctsou2377 <pctsou2377@gmail.com>
2026-07-01 08:42:31 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nutrition-pwa-v1";
+const CACHE_NAME = "nutrition-pwa-bilingual-v1-1";
 const ASSETS = [
   "./",
   "./index.html",
@@ -9,6 +9,16 @@ const ASSETS = [
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
